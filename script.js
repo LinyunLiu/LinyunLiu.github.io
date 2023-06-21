1215c83b5424da98faceed14b0760fb4fd80eaa3
linyunliu <linyunliu574@gmail.com>
2023-06-21 03:34:14 -0700

    commit-jun21-0

---
+++ script.js
@@ -0,0 +1,18 @@
+function toCompPage(){
+    window.open("computing.html");
+}
+function toArtPage(){
+    window.open("artworks.html");
+}
+function toFilmPage(){
+    window.open("films.html");
+}
+function toMusicPage(){
+    window.open("musics.html");
+}
+function toPhotoPage(){
+    window.open("photo.html");
+}
+function toBlogPage(){
+    window.open("blogs.html");
+}
